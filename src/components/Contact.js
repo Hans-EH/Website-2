35e38700e0079e20395b664893048f43cb77f26f
Hans <hansheje@hotmail.com>
2021-09-11 14:29:58 +0200

    asd

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,9 +13,9 @@ const Greeting = () => {
         interesting offers or simply to ask a question, anything is welcomed.{" "}
       </h2>
 
-      <a href="mailto:Hansheje@hotmail.com" class="btnDown">Email</a>
-      <a  href="https://www.linkedin.com/in/hans-heje-529849199/" class="btnDown">LinkedIn</a>
-      <a href="https://github.com/Hans-EH" class="btnDown">Github</a>
+      <a href="mailto:Hansheje@hotmail.com" class="btnDown">Email</a>•	
+      <a href="https://www.linkedin.com/in/hans-heje-529849199/" class="btnDown">LinkedIn</a>•
+      <a href="https://github.com/Hans-EH" class="btnDown">Github</a>•
       <a href="Hans-CV.pdf" class="btnDown" download="Hans Heje Resume">Resume</a>
     </body>
   );
