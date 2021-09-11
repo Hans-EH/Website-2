6ce4370a8cc328d6f0354c690b335edf7d292cee
Hans <hansheje@hotmail.com>
2021-09-11 17:38:54 +0200

    as

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,11 +12,12 @@ const Greeting = () => {
         linkedIn and email mailbox are always available. Whether it be
         interesting offers or simply to ask a question, anything is welcomed.{" "}
       </h2>
-
+        <center>
       <a href="mailto:Hansheje@hotmail.com" class="btnDown">Email</a>•	
       <a href="https://www.linkedin.com/in/hans-heje-529849199/" class="btnDown">LinkedIn</a>•
       <a href="https://github.com/Hans-EH" class="btnDown">Github</a>•
       <a href="Hans-CV.pdf" class="btnDown" download="Hans Heje Resume">Resume</a>
+      </center>
     </body>
   );
 };
