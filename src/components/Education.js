04f2becba5a163e62dfcbaa457856aef630f96a4
Hans <hansheje@hotmail.com>
2021-09-12 18:55:59 +0200

    asd

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,13 +12,16 @@ const Education = () => {
           <h2>
             <strong>2020-2023</strong>
           </h2>
+          <br/>
           <h2>
             <strong>2016-2019</strong>
           </h2>
         </div>
         <div class="column3">
-          <h2>Software Engineering, GPA 3.7; grade 10.5</h2>
-          <h2>
+        <h2 style={{marginBottom: "0%"}}><span>Aalborg University</span></h2>
+          <h2 style={{marginTop: "0%", marginBottom: "5%"}}>Software Engineering, GPA 3.7; grade 10.5</h2>
+          <h2 style={{marginBottom: "0%"}}><span>Albertslund Gymnasium</span></h2>
+          <h2 style={{marginTop: "0%", marginBottom: "5%"}}>
             Math, Biotech., Robot-tech. and Chemistry A-levels: GPA 3.7; Grade 10.5
           </h2>
         </div>
