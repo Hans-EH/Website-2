8b95c47f6a3554864a9d13f43187d9f72cbceb84
Hans <hansheje@hotmail.com>
2021-09-24 18:50:25 +0200

    asd

--- src/components/About.js
+++ src/components/About.js
@@ -3,18 +3,7 @@ import "../App.css";
 const About = () => {
   return (
     <body>
-      <Helmet>
       <title>Who am I?</title>
-      <meta name="description" content="An ambitious Danish boy born in 1999 currently residing in the
-            capital region of Denmark. My passion is anything data related:
-            Visualizing, extrapolating, and securing the data. Therefore, it is
-            fitting that I’m currently doing a BSc in Software engineering. My
-            primary technological competencies lie in the following areas: OOP
-            (Java, C#, and Python), full stack development (MERN stack, SQL),
-            digital electronics (C) and general data science. All of which I
-            have applied in large solo- or group projects consisting of 6-7
-            people."></meta>
-      </Helmet>
       <h1 style={{ marginBottom: "0%" }}>
         <span>01.</span> About me
       </h1>
