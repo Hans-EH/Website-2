8370da81796f83d55a1f7bf749395508fd7f57ac
Hans Heje <hansheje@hotmail.com>
2021-12-23 18:41:42 +0100

    greeting updated

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -8,10 +8,10 @@ const Greeting = (props) => {
             <h2 style={{marginBottom: '0em', paddingLeft: '1%',color:'var(--highlightText)'}}>Hi, my name is</h2>
             </div>
             <div class="fade-in2">
-            <text style={{ fontSize: '7em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', padding: "0em" }}>Hans Heje</text>
+            <h2 style={{ fontSize: '7em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', padding: "0em",color:'var(--importantText)' }}>Hans Heje</h2>
             </div>
             <div class="fade-in4">
-            <text style={{paddingLeft: "1%", fontSize: '1.5em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', paddingTop: "0em", paddingBottom: "0em"}}>Stud. Software Engineer</text>
+            <text style={{paddingLeft: "1%", fontSize: '1.5em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', paddingTop: "0em", paddingBottom: "0em",color:'var(--importantText)'}}>Stud. Software Engineer</text>
             </div>
             <div class="fade-in3">
             <h2 style={{paddingLeft: '1%'}}>I'm a data enthusiast; cryptology lover; aspiring machine learning engineer. Currently working as an <a href="#experience"><span> student cyber security engineer </span></a> at Januar</h2>
