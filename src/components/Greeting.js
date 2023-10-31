daf12d01dfc1302992b07633ee4a2b64313355eb
Hans <hansheje@hotmail.com>
2023-10-31 22:40:19 +0100

    greeting

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -11,7 +11,7 @@ const Greeting = (props) => {
             <h2 style={{ fontSize: '7em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', padding: "0em",color:'var(--importantText)' }}>HANS HEJE</h2>
             </div>
             <div class="fade-in4">
-            <text style={{paddingLeft: "1%", fontSize: '1.5em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', paddingTop: "0em", paddingBottom: "0em",color:'var(--importantText)'}}>Stud. Software Engineer</text>
+            <text style={{paddingLeft: "1%", fontSize: '1.5em',  fontFamily: '"Copperplate", fantasy', marginTop: '0em',marginBottom: '0em', paddingTop: "0em", paddingBottom: "0em",color:'var(--importantText)'}}>Stud. Computer Science and Engineering</text>
             </div>
             <div class="fade-in3">
             <h2 style={{paddingLeft: '1%'}}>I'm a data enthusiast; cryptology lover; machine learning nerd. Currently working as an <a href="#experience"><span> student software engineer </span></a> at IBM</h2>
